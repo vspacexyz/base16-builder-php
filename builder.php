0c93eef28b3003abd12ed7d9e299b280051d22fe
Kevin Hamer <kevin@imarc.com>
2016-10-22 13:57:26 -0400

    code formatting of builder.php

--- builder.php
+++ builder.php
@@ -19,8 +19,15 @@ $builder = new Builder;
 $src_list = $builder->parse($sources_list);
 $sch_list = [];
 $tpl_list = [];
-if (file_exists($schemes_list)) $sch_list = $builder->parse($schemes_list);
-if (file_exists($templates_list)) $tpl_list = $builder->parse($templates_list);
+
+if (file_exists($schemes_list)) {
+	$sch_list = $builder->parse($schemes_list);
+}
+
+if (file_exists($templates_list)) {
+	$tpl_list = $builder->parse($templates_list);
+}
+
 /**
  * Switches between functions based on supplied argument
  */
@@ -57,49 +64,53 @@ switch (@$argv[1]) {
 	* Build all themes and schemes
 	*/
 	default:
-    if (count($sch_list) == 0) echo "Warning: Could not parse schemes or missing $schemes_list, did you do `php $0 update`?\n";
-    if (count($tpl_list) == 0) echo "Warning: Could not parse templates or missing $templates_list, did you do `php $0 update`?\n";
+	if (count($sch_list) == 0) {
+		echo "Warning: Could not parse schemes or missing $schemes_list, did you do `php $0 update`?\n";
+	}
+	if (count($tpl_list) == 0) {
+		echo "Warning: Could not parse templates or missing $templates_list, did you do `php $0 update`?\n";
+	}
 
 
-		// Loop templates repositories
-		foreach ($tpl_list as $tpl_name => $tpl_url) {
+	// Loop templates repositories
+	foreach ($tpl_list as $tpl_name => $tpl_url) {
 
-			$tpl_confs = $builder->parse(
-				"templates/$tpl_name/templates/config.yaml");
+		$tpl_confs = $builder->parse(
+			"templates/$tpl_name/templates/config.yaml");
 
-			// Loop template files
-			foreach ($tpl_confs as $tpl_file => $tpl_conf) {
+		// Loop template files
+		foreach ($tpl_confs as $tpl_file => $tpl_conf) {
 
-				$file_path = "templates/$tpl_name/" 
-					. $tpl_conf['output'];
+			$file_path = "templates/$tpl_name/" 
+				. $tpl_conf['output'];
 
-				// Remove all previous output
-				array_map('unlink', glob(
-					$file_path. '/base16-*' . $tpl_conf['extension']
-				));
+			// Remove all previous output
+			array_map('unlink', glob(
+				$file_path. '/base16-*' . $tpl_conf['extension']
+			));
 
-				// Loop scheme repositories
-				foreach ($sch_list as $sch_name => $sch_url) {
+			// Loop scheme repositories
+			foreach ($sch_list as $sch_name => $sch_url) {
 
-					// Loop scheme files
-					foreach (glob("schemes/$sch_name/*.yaml") as $sch_file) {
+				// Loop scheme files
+				foreach (glob("schemes/$sch_name/*.yaml") as $sch_file) {
 
-						$sch_data = $builder->parse($sch_file);
-						$tpl_data = $builder->buildTemplateData($sch_data);
+					$sch_data = $builder->parse($sch_file);
+					$tpl_data = $builder->buildTemplateData($sch_data);
 
-						$file_name = 'base16-'.$tpl_data['scheme-slug']
-							. $tpl_conf['extension'];
-						
-						$render = $builder->renderTemplate(
-							"templates/$tpl_name/templates/$tpl_file.mustache",
-							 $tpl_data);
+					$file_name = 'base16-'.$tpl_data['scheme-slug']
+						. $tpl_conf['extension'];
+					
+					$render = $builder->renderTemplate(
+						"templates/$tpl_name/templates/$tpl_file.mustache",
+						 $tpl_data);
 
-						$builder->writeFile($file_path, $file_name, $render);
+					$builder->writeFile($file_path, $file_name, $render);
 
-						echo "Built $file_name\n";
-					}
+					echo "Built $file_name\n";
 				}
 			}
 		}
-		break;
+	}
+	break;
 }
