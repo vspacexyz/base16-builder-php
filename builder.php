45dca8c51b03eb2a6786f0cc61f7b29b1b1fbdbc
Kevin Hamer <kevin@imarc.com>
2016-10-22 19:50:07 -0400

    Fix whitespace and else formatting

--- builder.php
+++ builder.php
@@ -45,7 +45,7 @@ switch (@$argv[1]) {
 		echo "Base16 Builder PHP CLI\n";
 		echo "https://github.com/chriskempson/base16-builder-php\n";
 		break;
-	
+
 	/**
 	* Updates template and scheme sources
 	*/
@@ -69,53 +69,53 @@ switch (@$argv[1]) {
 	* Build all themes and schemes
 	*/
 	default:
-	if (count($sch_list) == 0) {
-		echo "Warning: Could not parse schemes or missing $schemes_list, did you do `php ${argv[0]} update`?\n";
-	}
-	if (count($tpl_list) == 0) {
-		echo "Warning: Could not parse templates or missing $templates_list, did you do `php ${argv[0]} update`?\n";
-	}
+		if (count($sch_list) == 0) {
+			echo "Warning: Could not parse schemes or missing $schemes_list, did you do `php ${argv[0]} update`?\n";
+		}
+		if (count($tpl_list) == 0) {
+			echo "Warning: Could not parse templates or missing $templates_list, did you do `php ${argv[0]} update`?\n";
+		}
 
 
-	// Loop templates repositories
-	foreach ($tpl_list as $tpl_name => $tpl_url) {
+		// Loop templates repositories
+		foreach ($tpl_list as $tpl_name => $tpl_url) {
 
-		$tpl_confs = Builder::parse(
-			"templates/$tpl_name/templates/config.yaml");
+			$tpl_confs = Builder::parse(
+				"templates/$tpl_name/templates/config.yaml");
 
-		// Loop template files
-		foreach ($tpl_confs as $tpl_file => $tpl_conf) {
+			// Loop template files
+			foreach ($tpl_confs as $tpl_file => $tpl_conf) {
 
-			$file_path = "templates/$tpl_name/" 
-				. $tpl_conf['output'];
+				$file_path = "templates/$tpl_name/"
+					. $tpl_conf['output'];
 
-			// Remove all previous output
-			array_map('unlink', glob(
-				$file_path. '/base16-*' . $tpl_conf['extension']
-			));
+				// Remove all previous output
+				array_map('unlink', glob(
+					$file_path. '/base16-*' . $tpl_conf['extension']
+				));
 
-			// Loop scheme repositories
-			foreach ($sch_list as $sch_name => $sch_url) {
+				// Loop scheme repositories
+				foreach ($sch_list as $sch_name => $sch_url) {
 
-				// Loop scheme files
-				foreach (glob("schemes/$sch_name/*.yaml") as $sch_file) {
+					// Loop scheme files
+					foreach (glob("schemes/$sch_name/*.yaml") as $sch_file) {
 
-					$sch_data = Builder::parse($sch_file);
-					$tpl_data = $builder->buildTemplateData($sch_data);
+						$sch_data = Builder::parse($sch_file);
+						$tpl_data = $builder->buildTemplateData($sch_data);
 
-					$file_name = 'base16-'.$tpl_data['scheme-slug']
-						. $tpl_conf['extension'];
-					
-					$render = $builder->renderTemplate(
-						"templates/$tpl_name/templates/$tpl_file.mustache",
-						 $tpl_data);
+						$file_name = 'base16-'.$tpl_data['scheme-slug']
+							. $tpl_conf['extension'];
 
-					$builder->writeFile($file_path, $file_name, $render);
+						$render = $builder->renderTemplate(
+							"templates/$tpl_name/templates/$tpl_file.mustache",
+							 $tpl_data);
 
-					echo "Built $file_name\n";
+						$builder->writeFile($file_path, $file_name, $render);
+
+						echo "Built $file_name\n";
+					}
 				}
 			}
 		}
-	}
-	break;
+		break;
 }
--- src/Builder.php
+++ src/Builder.php
@@ -6,21 +6,21 @@
  * Follows the conventions at
  *     http://chriskempson.com/projects/base16/builder.md
  */
- 
+
 namespace Base16;
 
 use Symfony\Component\Yaml\Yaml;
 use Mexitek\PHPColors\Color;
 use Cocur\Slugify\Slugify;
 
-class Builder 
+class Builder
 {
 	private $slugify;
 
 	/**
 	 * Parses a YAML file
 	 */
-	static public function parse($path) 
+	static public function parse($path)
 	{
 		return Yaml::parse( file_get_contents($path) );
 	}
@@ -28,7 +28,7 @@ class Builder
 	/**
 	 * Constructor
 	 */
-	public function __construct() 
+	public function __construct()
 	{
 		$this->slugify = new Slugify();
 	}
@@ -36,56 +36,57 @@ class Builder
 	/**
 	 * Uses git to fetch template or scheme sources
 	 */
-	public function fetchSources($url_list, $path) 
+	public function fetchSources($url_list, $path)
 	{
 		foreach ($url_list as $name => $url) {
-			if (!file_exists("$path/$name")) { 
+			if (!file_exists("$path/$name")) {
 				exec("git clone $url $path/$name\n");
 			}
 		}
 	}
-	
+
 	/**
 	 * Uses git to update template or scheme sources
 	 */
-	public function updateSources($url_list, $path) 
+	public function updateSources($url_list, $path)
 	{
 		foreach ($url_list as $name => $url) {
 			echo "\n-- $path/$name\n";
-			
+
 			if (file_exists("$path/$name")) {
 				echo exec("git -C $path/$name pull\n") . "\n";
+			} else {
+				$this->fetchSources($url_list, $path);
 			}
-			else $this->fetchSources($url_list, $path);
 		}
 	}
-	
+
 	/**
 	 * Renders a template using Mustache
 	 */
-	public function renderTemplate($path, $template_data) 
+	public function renderTemplate($path, $template_data)
 	{
 		$mustache = new \Mustache_Engine();
 		$tpl = $mustache->loadTemplate($this->readFile($path));
 		return $tpl->render($template_data);
 	}
-	
+
 	/**
 	 * Uses git to fetch template or scheme sources
 	 */
-	public function buildTemplateData($scheme_data) 
+	public function buildTemplateData($scheme_data)
 	{
 		$vars['scheme-name'] = $scheme_data["scheme"];
 		$vars['scheme-author'] = $scheme_data["author"];
     	$vars['scheme-slug'] = $this->slugify($scheme_data["scheme"]);
-		
+
 		$bases = array('00', '01', '02', '03', '04', '05', '06', '07', '08',
 			'09', '0A', '0B', '0C', '0D', '0E', '0F');
-		
+
 		foreach ($bases as $base) {
 			$base_key = 'base' . $base;
 			$color = new Color($scheme_data[$base_key]);
-			
+
 			$vars[$base_key . '-hex'] = $color->getHex();
 			$vars[$base_key . '-hex-r'] = substr($color->getHex(), 0, 2);
 			$vars[$base_key . '-hex-g'] = substr($color->getHex(), 2, 2);
@@ -94,14 +95,14 @@ class Builder
 			$vars[$base_key . '-rgb-g'] = $color->getRgb()['G'];
 			$vars[$base_key . '-rgb-b'] = $color->getRgb()['B'];
 		}
-		
+
 		return $vars;
 	}
-	
+
 	/**
 	 * Reads a file
 	 */
-	public function readFile($path) 
+	public function readFile($path)
 	{
 		return file_get_contents($path);
 	}
@@ -109,7 +110,7 @@ class Builder
 	/**
 	 * Writes a file
 	 */
-	public function writeFile($file_path, $file_name, $contents) 
+	public function writeFile($file_path, $file_name, $contents)
 	{
 		if (!is_dir($file_path)) mkdir($file_path);
 		file_put_contents($file_path . '/' . $file_name, $contents);
@@ -118,9 +119,9 @@ class Builder
 	/**
 	 * Slugify a string
 	 */
-	public function slugify($string) 
+	public function slugify($string)
 	{
 		return $this->slugify->slugify($string);
 	}
-	
+
 }
